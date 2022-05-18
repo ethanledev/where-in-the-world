5ee3ef4d35c459213e5032ff7925ccc18d49d9b6
Hieu Le <hieuhmle@gmail.com>
2022-05-18 16:23:07 -0500

    Add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import styles from "./App.module.css";
+import CountryPage from "./CountryPage";
+import HomePage from "./HomePage";
 
 const App = () => {
-  return <div className="App"></div>;
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <div className={styles.container} data-theme={theme}>
+      <header className={styles.header}>
+        <h1>Where in the world?</h1>
+        <div className={styles.themeButton} onClick={toggleTheme}>
+          <img
+            src={theme === "light" ? "/dark-mode.png" : "/light-mode.png"}
+            alt={theme === "light" ? "moon icon" : "sun icon"}
+          />{" "}
+          {theme === "light" ? "Dark Mode" : "Light Mode"}
+        </div>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path=":id" element={<CountryPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/CountryPage.js
@@ -0,0 +1,7 @@
+import styles from "./CountryPage.module.css";
+
+const CountryPage = () => {
+  return <div>CountryPage</div>;
+};
+
+export default CountryPage;
